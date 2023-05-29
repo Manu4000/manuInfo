3801cb6a936a46143ca406e19d280cc53728ab0c
Manu4000 <manolitotorres.2020@gmail.com>
2023-05-29 14:10:09 -0300

    correcciones de responsive

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import about from "./img/about_Manu.png";
-
+import "./Styles.css"; 
 export function About() {
   return (
     <main className="background d-flex" id="about">
       <Container>
         <Row className="img-fluid">
           <Col
-            lg={7}
+            lg={8}
             md={12}
             sm={12}
             className="secciones my-5"
             data-aos="fade-up-right"
           >
-            <h2 className="text-light text-center mx-5 my-5"><b>Acerca de mí</b></h2>
-            <div>
-              <p className="text-light text-center mx-5 my-5">
+            <h2 className="text-light text-center mx-3 mx-sm-5 my-4"><b>Acerca de mí</b></h2>
+            <div className="text-center mx-3 mx-sm-5 my-4">
+              <p className="text-light">
                 Un apasionado programador Full Stack. Comencé mi viaje en el mundo
                 del desarrollo web en octubre de 2022 y desde entonces he
                 adquirido una sólida formación en diferentes tecnologías y
                 herramientas.
               </p>
-              <p className="text-light text-center mx-5 my-5">
+              <p className="text-light">
                 Mi enfoque principal se centra en crear aplicaciones web
                 eficientes y funcionales, tanto en el frontend como en el backend.
                 Con habilidades en lenguajes de programación como JavaScript,
                 HTML, CSS, y experiencia en frameworks como React y Node.js.
               </p>
-              <p className="text-light text-center mx-5 my-5">
+              <p className="text-light">
                 Estoy preparado para enfrentar desafíos y desarrollar soluciones
                 creativas. Me destaco por mi capacidad para trabajar en equipo,
                 comunicarme efectivamente y aprender de manera continua. Estoy
@@ -37,7 +37,16 @@ export function About() {
               </p>
             </div>
           </Col>
-  
+          <Col
+            lg={4}
+            md={12}
+            sm={12}
+            className="secciones d-flex justify-content-center align-items-center"
+          >
+            <div className="img-fluid d-flex justify-content-center align-items-center" >
+              <img src={about} alt="" data-aos="fade-up-left" className="img-fluid responsive-image" />
+            </div>
+          </Col>
         </Row>
       </Container>
     </main>
--- src/Components/fondoPrincipal.js
+++ src/Components/fondoPrincipal.js
@@ -10,10 +10,10 @@ function Fondo() {
         <header className="background d-flex">
             <Container>
                 <Row>
-                    <Col className="col-lg-4" >
+                    <Col className="col-lg-4 col-md-12 col-sm-12" >
                         <img src={manu} className="manu" alt="Imagen de Manuel Torres" data-aos="fade-up-right" /></Col>
 
-                    <Col className="col-lg-7">
+                    <Col className="col-lg-7 col-md-12 col-sm-12">
                         <div>
                             <strong>
                                 <p className="text-light presentacion text-center" data-aos="fade-up-left" > ¡Hola!  soy Manuel Torres Desarrollador Full Stack, Bienvenido!
